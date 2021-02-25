ffab42c1342e44368a26e124b73a34f84749e2ab
Alex Curnow <Alex.Curnow@cot.tn.gov>
2021-02-25 16:20:41 -0600

    Initial Commit

---
+++ FlappyBirdDemo.Web/Models/BirdModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlappyBirdDemo.Web.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGound { get; set; } = 100;
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGound -= gravity;
+        }
+    }
+}
---
+++ FlappyBirdDemo.Web/Models/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlappyBirdDemo.Web.Models
+{
+    public class GameManager : INotifyPropertyChanged
+    {
+        private readonly int _gravity = 2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BirdModel Bird { get; set; }
+        public bool IsRunning { get; set; } = false;
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+
+            while (IsRunning)
+            {
+                Bird.Fall(2);
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bird)));
+
+                if (Bird.DistanceFromGound <= 0)
+                {
+                    GameOver();
+                }
+
+                await Task.Delay(20);
+            }
+        }
+
+        public void StartGame()
+        {
+            if (!IsRunning)
+            {
+
+                Bird = new BirdModel();
+                MainLoop();
+
+            }
+        }
+
+        public void GameOver()
+        {
+            IsRunning = false;
+        }
+    }
+}
